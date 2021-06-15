d4e518735908cd87512e9de973eb7c51f0745d27
akhilmhdh <akhilmhdh@gmail.com>
2021-06-16 01:03:56 +0530

    refactor(1230): LH-SPA changed home component to module

--- packages/lighthouse-spa/src/app/app.module.ts
+++ packages/lighthouse-spa/src/app/app.module.ts
@@ -3,25 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import '@one-platform/opc-footer/dist/opc-footer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    GraphQLModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, GraphQLModule, HttpClientModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ packages/lighthouse-spa/src/app/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { HomeRoutingModule } from './home-routing.module';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, FormsModule, HomeRoutingModule],
+})
+export class HomeModule {}
